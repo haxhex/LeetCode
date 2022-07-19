d866d443915ebac000b1b5951807a33a8a63ba18
Helia Ghorbani <heliaghorbani2001@gmail.com>
2022-07-19 12:28:27 +0430

    change all the progarm 1

--- PalindromeLinkedList.py
+++ PalindromeLinkedList.py
@@ -1,29 +1,50 @@
-
-# A linkesd list Node
-from django.urls import Resolver404
+# A Linked Lisy Node
+from unittest import result
 
 
 class Node:
-    def __init__(self, data, next=None):
+    # Node constructor just have data and point to next node (a pointrer)
+    def __init__(self, data, next = None):
         self.data = data
         self.next = next
-# Rec func tgo check a palindrome or not
+# check nodes from left and right to check if Palindrome or not        
 def checkPalindrome(left, right):
+    # base case
+    # consider none of nodes are remind
+    # end of the nodes
     if right is None:
         return True, left
-    val, left = checkPalindrome(left, right.next)
+    
+    # consider moving from left to right
+    # so the next node would be the right node
+    val, left = checkPalindrome(left, right = next)
+    
+    # the result is true when left data is equal to right data
     result = val and (left.data == right.data)
-    left = left.next 
+    
+    # move the pointer from left
+    left = left.next
+    
+    # left just use when see all nodes (ath the end of array)
     return result, left
-def checkPalin(head):
+    
+# func to check the linked list is a palindrome pr not
+def checkPlain(head):
     return checkPalindrome(head, head)[0]
 
 if __name__ == '__main__':
-    #input keys
-    keys = [1, 3, 5, 3, 1]
-    head = None
-    for i in reversed(range(len(keys))):\
-        head = Node(keys[i], head)
     
-    if (c)
+    # input keys
+    keys = [1, 3, 5, 3, 1] # True
+    
+    head = None
+    
+    for i in  reversed(range(len(keys))):
+        head = Node(keys[i], head)
         
+    if checkPlain(head):
+        print("is palindrome")
+    else:
+        print("not palindrome")
+
+
