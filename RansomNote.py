2ddb0da6c9de4b99002480546839ce4900d03533
Helia Ghorbani <heliaghorbani2001@gmail.com>
2022-07-20 11:39:29 +0430

    add description for ransomNotes

--- RansomNote.py
+++ RansomNote.py
@@ -6,12 +6,15 @@ f = 0
 ran = sorted(ransomNote)
 mag = sorted(magazine)
 
-# cout each letter in ransomNote
+# count each letter in ransomNote
 for r in ran:
     val = ran.count(r)
     d[r] = val
 print(d)
 
+# compare number of each letter in magazine to letters
+# and number of items in dictionary created according to
+# ransomNote
 for k, v in d.items():
     if k in mag and mag.count(k) >= v:
         pass
